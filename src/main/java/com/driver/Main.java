f83b574a222c9c3567257d5120b7e55debc93886
shravan kumar <sravan3251@gmail.com>
2025-01-14 14:19:33 +0530

    Solved encapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly rwonly = new RWOnly();
+        rwonly.setName("Shravankumar");
+        //'name' has private access in 'com. driver. RWOnly'
+       // System.out.println(rwonly.name);
+        System.out.println(rwonly.getName());
+
+
+    }
   
 }
